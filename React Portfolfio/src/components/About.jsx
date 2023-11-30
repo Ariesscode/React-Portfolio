2f051d89937fb1486f0670cd24ccd638d31b79ff
Abigail Burtley <abigailtop95@yahoo.com>
2023-11-30 10:53:33 -0500

    add about me

--- React Portfolfio/src/components/About.jsx
+++ React Portfolfio/src/components/About.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 function About() {
     return (
-      <div>
-        <h2>Home Page Content</h2>
+        <div>
+        <h1>About me</h1>
+        <p>
+          Full stack MERN web developer. I enjoy challeneges and expanding my knowledge in different activities. I believe if you work towards something and continue to put your all into it, you will achieve greater things in life. I am a person that can become a pro at something with just the right coaches and leaders. I am a leader myself, I have managed employees, been awarded for strong leadership skills and problem  solving. I guess that just comes from my mother being in the military for 22 years. I have moved around most of my life and still looking for my passion or should I say I have passiion for many things, I wish I could do it all at once! I was taking business courses at Valencia community college for two years and received my operations management certificate. I decieded to join the coding bootcamp that Univeristy of Central Florida offers because I have always been intrested in creating my own websites and also looking to make it into a career. Either way, I could do freelance or work for a tech job. Both sound like a good long term career and I love that I can continue to learn something new, because if you know, then you know that being a web developer, there is always something new to learn. I can go on about myself, but i should let you continue to check out my portfolio! I appreciate it and feel free to send me a message!
+        </p>
       </div>
     );
   }
