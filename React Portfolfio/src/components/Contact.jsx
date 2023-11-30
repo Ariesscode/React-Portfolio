810a23388497cc8f2e1dd5815ffd8131f7bd5ba7
Abigail Burtley <abigailtop95@yahoo.com>
2023-11-30 11:55:38 -0500

    add contact form style, signed up for emailjs to test to send form and email

--- React Portfolfio/src/components/Contact.jsx
+++ React Portfolfio/src/components/Contact.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
-function Contact() {
-    return (
-      <div>
-        <h2>Home Page Content</h2>
-      </div>
-    );
-  }
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import '../styles/contact-form.css';
+
+export const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('snervice_2kj8qx', 'template_nmjniar', form.current, '0F7q40iXe__uTE6yp')
+      .then((result) => {
+          console.log(result.text);
+          console.log('message sent.')
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail} className="contact-form">
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
 export default Contact;
+
+
