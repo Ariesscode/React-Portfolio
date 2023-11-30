d4ffaeed5608de23b1199a7b027594d7f5cb1ccc
Abigail Burtley <abigailtop95@yahoo.com>
2023-11-30 12:20:10 -0500

    add icons to contact

--- React Portfolfio/src/components/Contact.jsx
+++ React Portfolfio/src/components/Contact.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import '../styles/contact-form.css';
+import { SiMinutemailer } from "react-icons/si";
+import { FaPhoneAlt } from "react-icons/fa";
+
+
 
 export const Contact = () => {
   const form = useRef();
@@ -18,17 +22,35 @@ export const Contact = () => {
       });
   };
 
-  return (
-    <form ref={form} onSubmit={sendEmail} className="contact-form">
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
-  );
+  
+    return (
+      <div className='contact-form-edit'>
+        <div className="contact-info">
+          <p>For inquiries, contact us at:</p>
+          <p><FaPhoneAlt />
+Phone: (407) 690-0081</p>
+          <p><SiMinutemailer />
+Email: abigailtop95@yahoo.com</p>
+        </div>
+    
+        <form ref={form} onSubmit={sendEmail} className="contact-form">
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form>
+    
+        <div className="additional-links">
+          <a href="/privacy-policy">Privacy Policy</a>
+          <a href="/terms-and-conditions">Terms and Conditions</a>
+        </div>
+      </div>
+    );
+    
+  
 };
 export default Contact;
 
