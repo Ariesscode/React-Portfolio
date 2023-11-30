9ab402c379ca204e57a3b7c9af43058c3dd80dd2
Abigail Burtley <abigailtop95@yahoo.com>
2023-11-30 04:16:49 -0500

    add pathing for nav links

--- React Portfolfio/src/App.jsx
+++ React Portfolfio/src/App.jsx
@@ -12,7 +12,7 @@ import CustomCard from './components/profile-image';
 import portfolioImage from '../../Assets/portfolio.jpeg';
 
 import About from './components/About';
-import Portfolio from './components/projects';
+import Projects from './components/projects';
 import Contact from './components/Contact';
 import Resume from './components/Resume';
 
@@ -36,82 +36,66 @@ function App() {
   }, [isTyping]);
   return (
     <Router>
-    <div className="app-container">
-      <Nav />
-      <Footer />
-      <div style={{ display: 'flex' }}>
-      <Switch>
-            <Route path="/" exact>
-        <div className="text-effect" style={{ maxHeight: '700px', maxWidth: '620px', overflow: 'hidden' }}>
-          {isTyping && (
-            <Typewriter
-              options={{ loop: true, delay: 50 }}
-
-              onInit={(typewriter) => {
-                typewriter.typeString('<span class="typewriter">Hi, my name is Abigail.</span>')
-                  .pauseFor(2000)
-                  .deleteAll()
-
-                  .typeString('<span class="typewriter">Full stack MERN Developer...</span>')
-                  .pauseFor(3000)
-                  .deleteAll()
-                  .typeString('<span class="typewriter">Latest projects and collaborations.</span>')
-                  .pauseFor(2000)
-                  .deleteAll()
-                  .typeString('<span class="typewriter">Front-End skills...</span>')
-                  .pauseFor(2000)
-                  .deleteAll()
-
-                  .typeString('<span class="typewriter">Back-End skills...</span>')
-                  .pauseFor(2000)
-                  .deleteAll()
-
-                  .typeString('<span class="typewriter">MONGO, MYSQL, NODE</span>')
-                  .pauseFor(3000)
-                  .deleteAll()
-
-                  .typeString('<span class="typewriter">CSS, EXPRESS, HTML,</span>')
-                  .pauseFor(3000)
-                  .deleteAll()
-
-                  .typeString('<span class="typewriter">BOOTSTRAP...</span>')
-                  .pauseFor(3000)
-                  .deleteAll()
-
-                  .typeString('<span class="typewriter">JavaScript, REACT...</span>')
-                  .pauseFor(2000)
-                  .deleteAll()
-                  .start();
-
-
-              }}
-
+      <div className="app-container">
+        <Nav />
+        <Footer />
+        <div style={{ display: 'flex' }}>
+          <div className="text-effect" style={{ maxHeight: '700px', maxWidth: '620px', overflow: 'hidden' }}>
+            {isTyping && (
+              <Typewriter
+                options={{ loop: true, delay: 50 }}
+                onInit={(typewriter) => {
+                  typewriter.typeString('<span class="typewriter">Hi, my name is Abigail.</span>')
+                    .pauseFor(2000)
+                    .deleteAll()
+                    .typeString('<span class="typewriter">Full stack MERN Developer...</span>')
+                    .pauseFor(3000)
+                    .deleteAll()
+                    .typeString('<span class="typewriter">Latest projects and collaborations.</span>')
+                    .pauseFor(2000)
+                    .deleteAll()
+                    .typeString('<span class="typewriter">Front-End skills...</span>')
+                    .pauseFor(2000)
+                    .deleteAll()
+                    .typeString('<span class="typewriter">Back-End skills...</span>')
+                    .pauseFor(2000)
+                    .deleteAll()
+                    .typeString('<span class="typewriter">MONGO, MYSQL, NODE</span>')
+                    .pauseFor(3000)
+                    .deleteAll()
+                    .typeString('<span class="typewriter">CSS, EXPRESS, HTML,</span>')
+                    .pauseFor(3000)
+                    .deleteAll()
+                    .typeString('<span class="typewriter">BOOTSTRAP...</span>')
+                    .pauseFor(3000)
+                    .deleteAll()
+                    .typeString('<span class="typewriter">JavaScript, REACT...</span>')
+                    .pauseFor(2000)
+                    .deleteAll()
+                    .start();
+                }}
+              />
+            )}
+          </div>
+          <div className="fixed-card" style={{
+            position: 'fixed', top: '100px', right: '80px'
+          }}>
+            <CustomCard
+              imageUrl={portfolioImage}
+              description="It's just the beginning! Thanks for stopping by to check out my portfolio."
+              buttonText="Read more"
+              buttonUrl="https://example.com" //url for button to go to about page 
+              className="custom-card"
             />
-
-          )}
-
-        </div>
-        
-        </Route>
-
-<Route path="/About" component={About} />
-<Route path="/projects" component={Projects} />
-<Route path="/Contact" component={Contact} />
-<Route path="/Resume" component={Resume} />
-</Switch>
-        <div className="fixed-card" style={{
-          position: 'fixed', top: '100px', right: '80px'
-        }}>
-          <CustomCard
-            imageUrl={portfolioImage}
-            description="It's just the beginning! Thanks for stopping by to check out my portfolio."
-            buttonText="Read more"
-            buttonUrl="https://example.com" //url for button to go to about page 
-            className="custom-card"
-          />
+          </div>
+          <Switch>
+            <Route path="/About" component={About} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/Contact" component={Contact} />
+            <Route path="/Resume" component={Resume} />
+          </Switch>
         </div>
       </div>
-    </div>
     </Router>
   );
 }
