9bb0c47fd7cb2fadb3d39a3c5803d76f9f42b168
Abigail Burtley <abigailtop95@yahoo.com>
2023-11-30 12:56:55 -0500

    push

--- React Portfolfio/src/components/Resume.jsx
+++ React Portfolfio/src/components/Resume.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { LuConstruction } from "react-icons/lu";
+import '../styles/resume.css'
+
 function Resume() {
     return (
       <div>
-        <h2>Home Page Content</h2>
+        <h2>uploading soon..</h2>
+        <LuConstruction className='soon-updated' />
+
       </div>
     );
   }
