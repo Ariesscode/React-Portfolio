3eadd5aaa6b352830b29db5aacc73b4a833b1198
Abigail Burtley <abigailtop95@yahoo.com>
2023-11-30 10:16:53 -0500

    add style to buttons

--- React Portfolfio/src/components/project-cards.jsx
+++ React Portfolfio/src/components/project-cards.jsx
@@ -5,18 +5,18 @@ import '../styles/projects.css';
 
 const ProjectCard = ({ imageUrl, title, description, linkUrl, linkName, repoLink }) => {
   return (
-    <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+    <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3 container-card">
       <div className="features-icons-icon d-flex">
         <img src={imageUrl} alt={title} className="m-auto" style={{width: '350px', height: '200px'}} />
       </div>
-      <h3>{title}</h3>
+     
       <p className="lead mb-0">{description}</p>
       <div className="project-button">
       <a href={linkUrl}>{linkName}</a>
       </div>
       <div className="github-repo">
         <a href={repoLink} target="_blank" rel="noopener noreferrer">
-          <TbReportSearch />  
+          <TbReportSearch className="github-repo-icon"/>  
         </a>
       </div>
     </div>
--- React Portfolfio/src/components/projects.jsx
+++ React Portfolfio/src/components/projects.jsx
@@ -12,7 +12,7 @@ function Projects() {
         <div className="container pt-4">
 
             <section className="features-icons bg-light text-center m-4">
-                <div className="container">
+                <div className="container container-edit">
                     <div className="row p-2">
                         <div className="col-lg-4">
                             <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
@@ -22,7 +22,6 @@ function Projects() {
 
                                 <ProjectCard
                                     imageUrl={projectImage2}
-                                    title="CMS Blog App"
                                     description="Create an account, share posts with friends using all CRUD operations!"
                                     linkUrl="https://cmsblog-app-616ab098b945.herokuapp.com/"
                                     linkName="CMS Blog"
@@ -39,7 +38,6 @@ function Projects() {
                                 </div>
                                 <ProjectCard
                                     imageUrl={projectImage3}
-                                    title="Note Taker App"
                                     description="Can't remember something? Take notes App is here to rescue!"
                                     linkUrl="https://takenotes0923-7072b42b7aee.herokuapp.com/"
                                     linkName="Note taker"
@@ -54,8 +52,8 @@ function Projects() {
                                 </div>
                                 <ProjectCard
                                     imageUrl={projectImage}
-                                    title="F2F Blog App"
-                                    description="Social app for web developers to share their ideas, snippets of code, and add resources!"
+                        
+                                    description="Social app for developers, share your ideas, snippets!"
                                     linkUrl="https://f2f-blog-ad8f414d0b59.herokuapp.com/"
                                     linkName="F2F Blog"
                                     repoLink="https://github.com/newprice247/F2F-Blog"
@@ -65,15 +63,7 @@ function Projects() {
                     </div>
                 </div>
             </section>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-                tenetur maiores, dolor iusto dolorum ullam, natus deleniti blanditiis
-                impedit suscipit sed magnam alias in, repellat expedita hic explicabo
-                architecto soluta. About us Lorem ipsum, dolor sit amet consectetur
-                adipisicing elit. Velit voluptate exercitationem quaerat pariatur
-                mollitia, excepturi, voluptatem eveniet a dolor nobis ex veniam totam
-                nostrum temporibus ad omnis nam rerum eligendi.
-            </p>
+           
         </div>
 
     );
